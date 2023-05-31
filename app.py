fa14166cd33f48b022068ca0a12cae2769829329
bymoen <alexb@innotechcollege.com>
2023-05-30 20:05:56 -0600

    added wikipedia interaction

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import random as ran
-import antigravity
 import helpers.log
+import wikipedia
+
+
+user_search = input("Which page would you like to see: ")
+try:
+    user_page = wikipedia.page(user_search)
+    print(user_page.title)
+    print(user_page.content)
+except IndexError:
+    print("Sorry, that page does not exist")
+except wikipedia.exceptions.PageError:
+    print("Sorry, that page does not exist")
+except:
+    print("Something went wrong. Sorry,")
+
 
 my_num = ran.randint(0,100)
 if(my_num >= 50):
@@ -12,3 +26,4 @@ else:
 helpers.log.log("What will this do?")
 helpers.log.log("Oh, that's neat")
 helpers.log.log("Test.")
+
